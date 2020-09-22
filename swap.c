bf918c08e706fb285453eac79faf73666d3a0c67
isgandarisgandarov <isgandar.isgandarov.std@bhos.edu.az>
2020-09-22 23:57:01 +0400

    Last update

--- swap.c
+++ swap.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 
-void swap(int *x, int *y) 
+void swap(int x, int y) 
 { 
-    int temp = *x; 
-    *x = *y; 
-    *y = temp; 
+    printf("Before Swapping: x = %d, y = %d\n", x, y);
+    int temp = x; 
+    x = y; 
+    y = temp;
+    printf("After Swapping: x = %d, y = %d\n", x, y);
 } 
   
 int main() 
@@ -13,8 +15,6 @@ int main()
     int x, y;
     x = 7;
     y = 10;
-    printf("Before Swapping: x = %d, y = %d\n", x, y);
-    swap(&x, &y); 
-    printf("After Swapping: x = %d, y = %d\n", x, y); 
+    swap(x, y); 
     return 0; 
 } 
