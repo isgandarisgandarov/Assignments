d5b31c2645f64099e8e191a0f507bad11ebf5101
isgandarisgandarov <isgandar.isgandarov.std@bhos.edu.az>
2020-10-05 19:12:05 +0400

    Update

--- WEEK3/assignment2.c
+++ WEEK3/assignment2.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-void swapStrings(char **destination, char **source);    /* I chose to use character pointers for strings not character arrays  */          
-														/* because using built in functions was not allowed */
-int main(){												/* also lengths of strings can be different */
-    char *str1 = "Karabagh is Azerbaijan";				/* which makes using for loop for swapping strings impossible. */
+/* I chose to use character pointers for strings not character arrays  */
+/* because using built in functions was not allowed */
+/* also lengths of strings can be different */
+/* which makes using for loop for swapping strings impossible. */
+
+void swapStrings(char **destination, char **source);              
+														
+int main(){												
+    char *str1 = "Karabagh is Azerbaijan";				
     char *str2 = "Azerbaijan";
 
     printf("str1: %s, str2: %s\n",str1,str2);
